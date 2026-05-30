54b66c25e10003cf20870824cca79648e643bc05
Vladyslava Badyda <vlada87.com@gmail.com>
2026-05-30 21:25:04 +0300

    hw

--- src/server.js
+++ src/server.js
@@ -14,35 +14,35 @@ app.use(express.json());
 app.use(pino());
 
 app.get('/notes', (req, res) => {
-    res.status(200).json({
-        message: 'Retrieved all notes',
-    });
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
 });
 
 app.get('/notes/:noteId', (req, res) => {
-    const { noteId } = req.params;
+  const { noteId } = req.params;
 
-    res.status(200).json({
-        message: `Retrieved note with ID: ${noteId}`,
-    });
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
 });
 
 app.get('/test-error', () => {
-    throw new Error('Simulated server error');
+  throw new Error('Simulated server error');
 });
 
 app.use((req, res) => {
-    res.status(404).json({
-        message: 'Route not found',
-    });
+  res.status(404).json({
+    message: 'Route not found',
+  });
 });
 
 app.use((err, req, res, next) => {
-    res.status(500).json({
-        message: err.message,
-    });
+  res.status(500).json({
+    message: err.message,
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
