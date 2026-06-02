9df1727252970c12ba7d1887a03bbba05ac8466a
Vladyslava Badyda <vlada87.com@gmail.com>
2026-06-02 23:32:11 +0300

    hw-4

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
+import { authenticate } from '../middleware/authenticate.js';
 
 import {
   getAllNotesSchema,
@@ -17,6 +18,7 @@ import {
 } from '../controllers/notesController.js';
 
 const router = Router();
+router.use(authenticate);
 
 router.get(
   '/notes',
--- src/server.js
+++ src/server.js
@@ -2,33 +2,33 @@ import { errors } from 'celebrate';
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
-
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.use(logger);
-
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 
-app.use(cors());
-
-app.use(notesRoutes);
+app.use('/auth', authRoutes);
+app.use('/notes', notesRoutes);
 
 app.use(notFoundHandler);
-
 app.use(errors());
-
 app.use(errorHandler);
 
 const bootstrap = async () => {
