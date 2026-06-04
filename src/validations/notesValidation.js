868f26d524dbe19cb47e270d38c7e4833174a0f0
Vladyslava Badyda <vlada87.com@gmail.com>
2026-06-04 16:57:29 +0300

    Update notesValidation.js

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,4 +1,4 @@
-import Joi from 'joi';
+import { Joi } from 'celebrate';
 import { Segments } from 'celebrate';
 import { isValidObjectId } from 'mongoose';
 import { TAGS } from '../constants/tags.js';
